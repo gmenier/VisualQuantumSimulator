c7e617165a25a8f620b4d4faf447c2e1ed7a5941
gmenier <gildas.menier@univ-ubs.fr>
2020-05-09 17:42:27 +0200

    main with example

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -11,18 +11,27 @@ import Vqs._
 object main {
 
   def main(args : Array[String]): Unit = {
-    //QUtils.removeImages
-    //QStudy.caseQFT()
 
-    val rr: QReg = QReg(4)
-    rr.trace(2)
+    import Vqs._
 
-    rr.init(1)
+    val rr = QReg(3)
 
-    rr - QFT()
+    rr.pdf("teleport.pdf"); rr.trace(2)
+
+    rr - H(1) - C(X(2), 1)
+    rr - H(0) - Rz(0, 45) - H(0)
+    rr - C(X(1), 0) - H(0)
+    rr - <(0) - <(1)
+
+    val alice = rr.readQbit(0)
+    val ep = rr.readQbit(1)
+    if (ep == 1) rr - X(2)
+    if (alice == 1) rr - Z(2)
+
+    rr - H(2) - Rz(2, -45) - H(2)
+    rr - <(2)
 
     rr.end()
-
 
 
 
