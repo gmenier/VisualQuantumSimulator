047c7920ad0871e8bd5bb48c9ffceb0259115fa7
gmenier <gildas.menier@univ-ubs.fr>
2021-02-13 11:39:22 +0100

    Fixed the pdf output

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -18,33 +18,3 @@ object main {
 
 }
 
-
-/*
-
-object main {
-
-  def main(args : Array[String]): Unit = {
-
-    // QUtils.removeImages
-
-    QReg.setDefaultDrawNOPhaseNormalization()
-
-    // QReg.setDefaultDrawAll()
-    // QReg.setDefaultUseASCII()
-
-    val rr: QReg = QReg(8)
-
-    rr.trace()
-
-    rr.init(1)
-    rr - H(0) - X(0) - Ry(0,-145) - C(X(1),0) - Rz(3,-195.45) - C(X(1),0) - <() //- C(X(1),0)
-
-
-    rr.end()
-
-
-
-  } // main
-
-} // Main
-*/
